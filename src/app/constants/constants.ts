e8ae01ec5ef48ddefb7c01493ac790edadf8d71f
Swiatlon <przemyslaw.swiatlon@gmail.com>
2024-11-26 10:47:16 +0100

    shelf

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -23,6 +23,43 @@ export const navLinks = [
 
 export const myProjects = [
   {
+    title: "University Management System",
+    desc: "A feature-rich platform designed to efficiently manage university operations, including student profiles, course details, and grade analysis. It provides a seamless user experience for students, faculty, and administrators.",
+    subdesc:
+      "Built using React, Node.js, SQL, and TypeScript, the system emphasizes scalability and usability. The backend handles complex queries, data migrations, and validations, while the frontend offers a responsive interface with detailed visuals, ensuring accessibility across devices. Key modules include academic information management, personalized profiles, course schedules, and performance dashboards.",
+    href: "https://university-swiatlons-projects.vercel.app/",
+    logo: "/assets/icons/react.svg",
+    logoStyle: {
+      backgroundColor: "#010202",
+      border: "0.2px solid #010202",
+      boxShadow: "0px 0px 40px 0px #007acc",
+    },
+    spotlight: "/assets/images/spotlight_university.png",
+    texture: "/assets/images/projects/university_dashboard.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/icons/react.svg",
+      },
+      {
+        id: 2,
+        name: "Node.js",
+        path: "/assets/icons/nodejs.svg",
+      },
+      {
+        id: 3,
+        name: "SQL",
+        path: "/assets/icons/sql.svg",
+      },
+      {
+        id: 4,
+        name: "TypeScript",
+        path: "/assets/icons/typescript.svg",
+      },
+    ],
+  },
+  {
     title: "Workout Website",
     desc: "An engaging landing page template is tailored to inspire fitness enthusiasts with motivational content and easy access to workout plans, enhancing their journey towards a healthier lifestyle. The responsive layout ensures that users have a seamless experience on any device, featuring vibrant visuals and clear navigation elements.",
     subdesc:
